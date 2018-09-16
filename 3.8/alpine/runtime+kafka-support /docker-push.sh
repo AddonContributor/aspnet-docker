354088164d87707f338eb80f3c99520e4e62e53d
Marc Lohrer <Marc.Lohrer@gmx.de>
2018-09-16 12:11:24 +0200

    fix: repository name in docker-push script

--- 3.8/alpine/runtime+kafka-support /docker-push.sh
+++ 3.8/alpine/runtime+kafka-support /docker-push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -x
-set -e
-docker build . -t lohrer/aspnetcore-alpine:latest \
-  && docker tag $(docker images --filter=reference='lohrer/aspnetcore-alpine:latest' -q) lohrer/aspnetcore-alpine+kafka-deps:alpine3.8 \
-  && docker tag $(docker images --filter=reference='lohrer/aspnetcore-alpine:latest' -q) lohrer/aspnetcore-alpine+kafka-deps:runtime-deps-alpine3.8 \
-  && docker push lohrer/aspnetcore-alpine+kafka-deps:runtime-deps-alpine3.8 \
-  && docker push lohrer/aspnetcore-alpine+kafka-deps:alpine3.8 \
-  && docker push lohrer/aspnetcore-alpine+kafka-deps:latest
+set -ex
+docker build . -t lohrer/aspnetcore-alpine-kafka:latest \
+  && docker tag $(docker images --filter=reference='lohrer/aspnetcore-alpine:latest' -q) lohrer/aspnetcore-alpine-kafka:alpine3.8 \
+  && docker tag $(docker images --filter=reference='lohrer/aspnetcore-alpine:latest' -q) lohrer/aspnetcore-alpine-kafka:runtime-alpine3.8 \
+  && docker push lohrer/aspnetcore-alpine-kafka:runtime-alpine3.8 \
+  && docker push lohrer/aspnetcore-alpine-kafka:alpine3.8 \
+  && docker push lohrer/aspnetcore-alpine-kafka:latest
